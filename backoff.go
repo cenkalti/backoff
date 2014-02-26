bea0f829f2a3d494f3fb6ba21a39b299b1611a0d
Cenk Alti <cenkalti@gmail.com>
2014-02-25 20:58:34 -0800

    add comments

--- backoff.go
+++ backoff.go
@@ -2,19 +2,39 @@ package backoff
 
 import "time"
 
+// Back-off policy when retrying an operation.
 type BackOff interface {
+	// Gets the number of milliseconds to wait before retrying the operation or {@link #STOP} to
+	// indicate that no retries should be made.
+	//
+	// Example usage:
+	//
+	// 	long backOffMillis = backoff.NextBackOffMillis();
+	// 	if (backOffMillis == Backoff.Stop) {
+	// 		// do not retry operation
+	// 	} else {
+	// 		// sleep for backOffMillis milliseconds and retry operation
+	// 	}
+	//
 	NextBackOff() time.Duration
+
+	// Reset to initial state.
 	Reset()
 }
 
+// Indicates that no more retries should be made for use in NextBackOffMillis().
 const Stop time.Duration = time.Duration(-1)
 
+// ZeroBackOff is a fixed back-off policy whose back-off time is always zero,
+// meaning that the operation is retried immediately without waiting.
 type ZeroBackOff struct{}
 
 func (b *ZeroBackOff) Reset() {}
 
 func (b *ZeroBackOff) NextBackOff() time.Duration { return 0 }
 
+// StopBackOff is a fixed back-off policy that always returns backoff.Stop for
+// NextBackOffMillis(), meaning that the operation should not be retried.
 type StopBackOff struct{}
 
 func (b *StopBackOff) Reset() {}
