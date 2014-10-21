6a458ae422d1a8cbfb686ac5f4789bd82956c61b
Cenk Alti <cenkalti@gmail.com>
2014-10-21 14:54:48 +0300

    add RetryNotify function

--- retry.go
+++ retry.go
@@ -18,7 +18,10 @@ import "time"
 //
 // 	// Operation is successfull.
 //
-func Retry(f func() error, b BackOff) error {
+func Retry(f func() error, b BackOff) error { return RetryNotify(f, b, nil) }
+
+// RetryNotify calls notify function with the error and wait duration for each failed attempt before sleep.
+func RetryNotify(f func() error, b BackOff, notify func(err error, wait time.Duration)) error {
 	var err error
 	var next time.Duration
 
@@ -30,6 +33,10 @@ func Retry(f func() error, b BackOff) error {
 
 		if next = b.NextBackOff(); next == Stop {
 			return err
+		}
+
+		if notify != nil {
+			notify(err, next)
 		}
 
 		time.Sleep(next)
