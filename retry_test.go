376009d63d4c4f05a416e0038f0560e3fde8e622
Arnaud Le Blanc <arnaud.lb@gmail.com>
2017-02-01 15:29:17 +0100

    Support contexts

--- retry_test.go
+++ retry_test.go
@@ -1,9 +1,12 @@
 package backoff
 
 import (
+	"context"
 	"errors"
+	"fmt"
 	"log"
 	"testing"
+	"time"
 )
 
 func TestRetry(t *testing.T) {
@@ -29,6 +32,40 @@ func TestRetry(t *testing.T) {
 		t.Errorf("unexpected error: %s", err.Error())
 	}
 	if i != successOn {
+		t.Errorf("invalid number of retries: %d", i)
+	}
+}
+
+func TestRetryContext(t *testing.T) {
+	var cancelOn = 3
+	var i = 0
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	// This function cancels context on "cancelOn" calls.
+	f := func() error {
+		i++
+		log.Printf("function is called %d. time\n", i)
+
+		// cancelling the context in the operation function is not a typical
+		// use-case, however it allows to get predictable test results.
+		if i == cancelOn {
+			cancel()
+		}
+
+		log.Println("error")
+		return fmt.Errorf("error (%d)", i)
+	}
+
+	err := Retry(f, WithContext(NewConstantBackOff(time.Millisecond), ctx))
+	if err == nil {
+		t.Errorf("error is unexpectedly nil")
+	}
+	if err.Error() != "error (3)" {
+		t.Errorf("unexpected error: %s", err.Error())
+	}
+	if i != cancelOn {
 		t.Errorf("invalid number of retries: %d", i)
 	}
 }
